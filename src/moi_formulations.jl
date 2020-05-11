d02e9ba78f0be8950da08cae3799d216ca89025d
fpacaud <francoispacaud8@gmail.com>
2020-05-11 18:07:11 +0200

    wip: add conic formulation for logistic regression

--- src/moi_formulations.jl
+++ src/moi_formulations.jl
@@ -63,6 +63,7 @@ function MOI.eval_hessian_lagrangian_product(d::LogisticEvaluator, h, x, v, σ, 
     hessvec!(view(h, 1:n), x[1:n], v[1:n], d.logreg)
 end
 
+# TODO: pass to a NormOneCone bridge
 function _linearize_penalty!(model::MOI.ModelLike,
                              parameters::Vector{MOI.VariableIndex},
                              logreg::LogisticRegressor)
@@ -94,9 +95,9 @@ function nlp_model(model::MOI.ModelLike, evaluator, logreg::LogisticRegressor)
     nvariables = length(logreg)
     # add variables
     start = zeros(nvariables)
-    v = MOI.add_variables(model, nvariables)
+    w = MOI.add_variables(model, nvariables)
     for i in 1:nvariables
-        MOI.set(model, MOI.VariablePrimalStart(), v[i], start[i])
+        MOI.set(model, MOI.VariablePrimalStart(), w[i], start[i])
     end
     # set NLP structure
     block_data = MOI.NLPBlockData([], evaluator, true)
@@ -104,7 +105,74 @@ function nlp_model(model::MOI.ModelLike, evaluator, logreg::LogisticRegressor)
     MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
 
     if isa(logreg.penalty, LinearizedL1Penalty)
-        _linearize_penalty!(model, v, logreg)
+        _linearize_penalty!(model, w, logreg)
     end
+    return w
+end
+
+function conic_model(model::MOI.ModelLike, logreg::LogisticRegressor)
+    MOI.empty!(model)
+    @assert MOI.supports_constraint(model, MOI.VectorAffineFunction{Float64}, MOI.ExponentialCone)
+
+    X = logreg.data.X
+    y = logreg.data.y
+
+    n = ndata(logreg.data)
+    nvariables = length(logreg)
+    # add variables
+    start = zeros(nvariables)
+    w = MOI.add_variables(model, nvariables)
+    t = MOI.add_variables(model, nvariables)
+
+    for j in 1:nvariables
+        MOI.set(model, MOI.VariablePrimalStart(), v[j], start[j])
+    end
+
+    for i in 1:n
+        # temporary variables
+        z = MOI.add_variables(model, 2)
+        # z1, z2 >= 0
+        MOI.add_constraint.(model, MOI.SingleVariable.(z), MOI.GreaterThan(0.0))
+        # z1 + z2 <= 1
+        c_sum = MOI.ScalarAffineFunction{Float64}(
+            MOI.ScalarAffineTerm{Float64}.([1.0, 1.0], z), 0.0)
+        MOI.add_constraint(model, c_sum, MOI.LessThan(1.0))
+
+        # (z1, 1, u - t ) ∈  K_exp
+        terms = MOI.VectorAffineTerm{Float64}[]
+        vat  = MOI.VectorAffineTerm{Float64}(3, MOI.ScalarAffineTerm{Float64}(1.0, z[1]))
+        push!(terms, vat)
+        vat  = MOI.VectorAffineTerm{Float64}(1, MOI.ScalarAffineTerm{Float64}(-1.0, t[i]))
+        push!(terms, vat)
+        for j in eachindex(X[i, :])
+            x_ij = X[i, j]
+            if x_ij != 0.0
+                x_ij *= y[i]
+                vat  = MOI.VectorAffineTerm{Float64}(1, MOI.ScalarAffineTerm{Float64}(x_ij, w[j]))
+                push!(terms, vat)
+            end
+        end
+
+        constants = [0.0, 1.0, 1.0]
+        vaf = MOI.VectorAffineFunction{Float64}(vat, constants)
+        vc = MOI.add_constraint(model, vaf, MOI.ExponentialCone())
+
+        # (z2, 1, - t ) ∈  K_exp
+        terms = MOI.VectorAffineTerm{Float64}[]
+        vat  = MOI.VectorAffineTerm{Float64}(3, MOI.ScalarAffineTerm{Float64}(1.0, z[2]))
+        push!(terms, vat)
+        vat  = MOI.VectorAffineTerm{Float64}(1, MOI.ScalarAffineTerm{Float64}(-1.0, t[i]))
+        push!(terms, vat)
+        constants = [0.0, 1.0, 1.0]
+        vaf = MOI.VectorAffineFunction{Float64}(vat, constants)
+        vc = MOI.add_constraint(model, vaf, MOI.ExponentialCone())
+    end
+
+    # add penalty
+    #
+    # add objective
+    ocoefs = fill(1.0 / n, nvariables)
+    objf = MOI.ScalarAffineFunction{Float64}(MOI.ScalarAffineTerm{Float64}.(ocoefs, t), 0.0)
+    MOI.set(model, MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), objf)
 end
 
